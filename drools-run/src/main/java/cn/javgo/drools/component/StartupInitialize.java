fab77adcdbb39bf03b6e685998d718d6144dedbf
javgo <javgocn@gmail.com>
2023-09-12 09:41:07 +0800

    add StartupInitialize.java

---
+++ drools-run/src/main/java/cn/javgo/drools/component/StartupInitialize.java
@@ -0,0 +1,12 @@
+package cn.javgo.drools.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统启动初始化数据类
+ */
+@Slf4j
+@Component
+public class StartupInitialize {
+}
