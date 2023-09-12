1d4c314556067fe2f96aef823dd1e07f700c61f5
javgo <javgocn@gmail.com>
2023-09-12 09:39:40 +0800

    update ApplicationTests

---
+++ drools-run/src/test/java/cn/javgo/drools/ApplicationTests.java
@@ -0,0 +1,7 @@
+package cn.javgo.drools;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DroolsRunApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class ApplicationTests {
+}
